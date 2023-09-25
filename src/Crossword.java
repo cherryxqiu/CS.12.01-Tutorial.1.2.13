64380fb088e210c9094333a9079d25f149088432
cherryqiu <xiaohuiq2025@student.cis.edu.hk>
2023-09-25 14:36:37 +0800

    Completed

---
+++ src/Crossword.java
@@ -0,0 +1,29 @@
+public class Crossword {
+    private Square[][] puzzle;
+
+    public Crossword (boolean[][] blackSquares) {
+        puzzle = new Square[blackSquares.length][blackSquares[0].length];
+        int cnt = 1;
+        for (int i = 0; i < blackSquares.length; i++) {
+            for (int j = 0; j < blackSquares[0].length; j++) {
+                puzzle[i][j] = new Square(blackSquares[i][j], 0);
+                if (toBeLabeled(i, j, blackSquares)) {
+                    puzzle[i][j].setNum(cnt++);
+                }
+            }
+        }
+
+    }
+
+    private boolean toBeLabeled (int r, int c, boolean[][] blackSquares) {
+        if (blackSquares[r][c]) return false;
+        if (r == 0 || c == 0) return true;
+        if(blackSquares[r-1][c] || blackSquares[r][c-1])return true;
+
+        return false;
+    }
+
+    public Square[][] getPuzzle() {
+        return this.puzzle;
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,25 @@
+public class Square {
+
+    private boolean isBlack;
+
+    private int num;
+
+    public Square (boolean isBlack, int num)
+    {
+        this.isBlack = isBlack;
+        this.num = num;
+
+    }
+
+    public void setNum (int num) {
+        this.num = num;
+    }
+
+    public boolean isBlack() {
+        return isBlack;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
